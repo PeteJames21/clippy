9e796ca934650c6735fcba0132f2685144797753
Gabogogi <bencitymaina@gmail.com>
2024-05-23 21:07:36 +0300

    Navbar edits

--- app/ui/dashboard/header.jsx
+++ app/ui/dashboard/header.jsx
@@ -5,10 +5,14 @@ import Image from "next/image"
 export default function Header() {
   return (
     <nav className={styles.header}>
-      <div className={styles.logo}>(●'◡'●)</div>
-      <div><input type="text" id="searchInput" className={styles["search-content"]} placeholder="Search for content..." /></div>
-      <div className={styles['header-right']}>
-        <Image src='/avatar.jpg' alt="Avatar" className={styles.avatar} width={50} height={50}/>
+      <div className={styles.logoContainer}>
+        <div className={styles.logo}>(●'◡'●)</div>
+      </div>
+      <div className={styles.searchContainer}>
+        <input type="text" id="searchInput" className={styles.searchContent} placeholder="Search for content..." />
+      </div>
+      <div className={styles.avatarContainer}>
+        <Image src='/avatar.jpg' alt="Avatar" className={styles.avatar} width={50} height={50} />
       </div>
     </nav>
   );
