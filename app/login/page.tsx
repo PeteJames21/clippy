0601e38330f8b2def091a1fb95be63eec78786c0
Gabogogi <bencitymaina@gmail.com>
2024-05-23 22:24:37 +0300

    Login and register form

---
+++ app/login/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+import React, {useState} from 'react'
+import AuthForm from "@/app/ui/forms/Login";
+import "@/app/ui/forms/Login.css";
+
+
+const App: React.FC = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleForm = () => {
+    setIsLogin(!isLogin);
+  };
+
+  return (
+    <div className="container">
+      {isLogin ? (
+        <AuthForm
+          formType="login"
+          toggleForm={toggleForm}
+          title="Login"
+          subtitle="Welcome back! Please login to your account."
+          buttonText="Login"
+          switchText="Don't have an account?"
+          switchLinkText="Register"
+        />
+      ) : (
+        <AuthForm
+          formType="register"
+          toggleForm={toggleForm}
+          title="Register"
+          subtitle="Create a new account."
+          buttonText="Register"
+          switchText="Already have an account?"
+          switchLinkText="Login"
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ app/ui/forms/Login.tsx
@@ -0,0 +1,55 @@
+// src/Log.tsx
+import React from 'react';
+import "./Login.css";
+
+
+interface AuthFormProps {
+  formType: 'login' | 'register';
+  toggleForm: () => void;
+  title: string;
+  subtitle: string;
+  buttonText: string;
+  switchText: string;
+  switchLinkText: string;
+}
+
+const AuthForm: React.FC<AuthFormProps> = ({
+  formType,
+  toggleForm,
+  title,
+  subtitle,
+  buttonText,
+  switchText,
+  switchLinkText,
+}) => {
+  return (
+    <div className="form-container">
+      <div className="form-header">
+        <h2>{title}</h2>
+        <p>{subtitle}</p>
+      </div>
+      <form>
+        <div className="input-group">
+          <label htmlFor={`${formType}-email`}>Email</label>
+          <input type="email" id={`${formType}-email`} required />
+        </div>
+        <div className="input-group">
+          <label htmlFor={`${formType}-password`}>Password</label>
+          <input type="password" id={`${formType}-password`} required />
+        </div>
+        {formType === 'register' && (
+          <div className="input-group">
+            <label htmlFor="register-confirm-password">Confirm Password</label>
+            <input type="password" id="register-confirm-password" required />
+          </div>
+        )}
+        <button type="submit" className="btn">{buttonText}</button>
+        <p className="switch-form">
+          {switchText} <a href="#" onClick={toggleForm}>{switchLinkText}</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default AuthForm;
